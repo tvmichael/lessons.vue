3b464638bc993935543f61e45e0cf56c2feae8c2
tvmichael <michael.tmv@outlook.com>
2021-12-19 15:27:20 +0200

    lesson 5

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks-adult-black.jpg',
-            inventory: 8
+            inventory: 8,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                    {id: 1, color:'black'},
+                    {id: 2, color:'white'},
+                ]
         }
     }
 });
