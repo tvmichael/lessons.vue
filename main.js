948c87d709f50140827c1cc31c2c3931414dd4fc
tvmichael <michael.tmv@outlook.com>
2021-12-19 16:05:03 +0200

    lesson 6

--- main.js
+++ main.js
@@ -6,9 +6,18 @@ const app = Vue.createApp({
             inventory: 8,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                    {id: 1, color:'black'},
-                    {id: 2, color:'white'},
-                ]
+                    {id: 1, color:'black', image: './assets/images/socks-adult-black.jpg'},
+                    {id: 2, color:'white', image: './assets/images/socks-adult-white.jpg'},
+                ],
+            cart: 0
+        }
+    },
+    methods:{
+        addToCart() {
+            this.cart += 1;
+        },
+        updateImage(image) {
+            this.image = image;
         }
     }
 });
