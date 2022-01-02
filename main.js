41a80a22f85638ce8cba2aacb4b9f0ad8aa80f4f
tvmichael <michael.tmv@outlook.com>
2022-01-02 16:01:49 +0200

    l0: components fundamentals

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
 const config = {
     data(){
-        return {
-            header: 'Vue id ready!',
-            newItem: '',
-            items: [
-             'one pets',
-             'two cats'
-            ],
-            newItemPriority: '',
-            iceCreamFlavors: [],
-            editItem: false,
-        }
-    },
-    methods:{
-        addNewItem(){
-            this.items.push(this.newItem);
-            this.newItem = "";
-        },
-        editItemBtn(value){
-            this.editItem = value;
-            this.newItem = '';
-
-        }
     }
 };
 
-var shoppingList = Vue.createApp(config).mount('#shopping-list');
+var vComponents = Vue.createApp(config).mount('#components');
