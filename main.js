2cedda710aec039d5ad8115aa5abe82d0c82924f
tvmichael <michael.tmv@outlook.com>
2022-01-02 16:10:52 +0200

    l1

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const config = {
     data(){
+        return {
+            titleLessonName:'Components fundamental!'
+        }
     }
 };
 
-var vComponents = Vue.createApp(config).mount('#components');
+var vComponents = Vue.createApp(config)
+    .component('click-counter', {
+        template: `<button @click="count++">{{count}}</button>`,
+        data(){
+            return{
+                count:0
+            }
+        }
+    })
+    .mount('#components');
