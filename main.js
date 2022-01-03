851353aac92c147d7cec48c3b676671a1f37be16
tvmichael <michael.tmv@outlook.com>
2022-01-03 11:29:31 +0200

    Component Lifecycle Hooks

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let PlanComponent = {
-    template: '#plan-template',
+let PlanPickerItemComponent = {
+    template: '#plan-picker-item-template',
     props: {
         name: {
             type:String,
@@ -7,29 +7,77 @@ let PlanComponent = {
             required: true
         },
         price: Number,
+        selected: {
+            type: Boolean,
+            default: false
+        }
     },
     data(){
         return{
-            count:0
+            count:0,
+        }
+    },
+    methods:{
+        select(){
+            this.selected = true;
+            this.$emit('select', this.name);
         }
     }
 }
 
 let PlanPickerComponent = {
-    components: {plan: PlanComponent},
+    components: {'plan-picker-item': PlanPickerItemComponent},
     template: '#plan-picker-template',
     data(){
         return {
-            plans: ['The single', 'The curious', 'The Addict']
+            plans: ['The single', 'The curious', 'The Addict'],
+            selectedPlan: null
         }
     },
     props:{
 
+    },
+    methods:{
+        selectPlan(plan){
+            this.selectedPlan = plan;
+        }
     }
 }
 
+let TextInputComponent = {
+    props: {
+      id:{
+          type: String,
+          default: ''
+      }
+    },
+    template: '#text-input-template',
+    data(){
+        return {
+            labelText: 'Input text'
+        }
+    },
+    beforeCreate(){
+        console.log('>> 1');
+    },
+    created(){
+        console.log('>> 2');
+    },
+    beforeMount(e){
+        console.log('>> 3');
+        console.log(this);
+    },
+    renderTracked({ key, target, type }) {
+        console.log('>> r');
+        console.log({ key, target, type });
+    }
+};
+
 const config = {
-    components: {'plan-picker': PlanPickerComponent},
+    components: {
+        'plan-picker': PlanPickerComponent,
+        'text-input': TextInputComponent
+    },
     data(){
         return {
             titleLessonName:'Components fundamental!',
