9c288795ee4e467ade495b963400a71b6b51966a
tvmichael <michael.tmv@outlook.com>
2022-01-03 12:30:23 +0200

    Component Slots

--- main.js
+++ main.js
@@ -1,89 +1,12 @@
-let PlanPickerItemComponent = {
-    template: '#plan-picker-item-template',
-    props: {
-        name: {
-            type:String,
-            default: 'Empty!',
-            required: true
-        },
-        price: Number,
-        selected: {
-            type: Boolean,
-            default: false
-        }
-    },
-    data(){
-        return{
-            count:0,
-        }
-    },
-    methods:{
-        select(){
-            this.selected = true;
-            this.$emit('select', this.name);
+var vSlots = Vue.createApp({
+
+})
+.component('todo-item', {
+    template: '#todo-item-template',
+    data() {
+        return {
+            done: false
         }
     }
-}
-
-let PlanPickerComponent = {
-    components: {'plan-picker-item': PlanPickerItemComponent},
-    template: '#plan-picker-template',
-    data(){
-        return {
-            plans: ['The single', 'The curious', 'The Addict'],
-            selectedPlan: null
-        }
-    },
-    props:{
-
-    },
-    methods:{
-        selectPlan(plan){
-            this.selectedPlan = plan;
-        }
-    }
-}
-
-let TextInputComponent = {
-    props: {
-      id:{
-          type: String,
-          default: ''
-      }
-    },
-    template: '#text-input-template',
-    data(){
-        return {
-            labelText: 'Input text'
-        }
-    },
-    beforeCreate(){
-        console.log('>> 1');
-    },
-    created(){
-        console.log('>> 2');
-    },
-    beforeMount(e){
-        console.log('>> 3');
-        console.log(this);
-    },
-    renderTracked({ key, target, type }) {
-        console.log('>> r');
-        console.log({ key, target, type });
-    }
-};
-
-const config = {
-    components: {
-        'plan-picker': PlanPickerComponent,
-        'text-input': TextInputComponent
-    },
-    data(){
-        return {
-            titleLessonName:'Components fundamental!',
-        }
-    },
-};
-
-var vComponents = Vue.createApp(config)
-    .mount('#components');
+})
+.mount('#app');
