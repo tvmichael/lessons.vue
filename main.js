1682c43172b493be52f0703df6d215665f3cddad
tvmichael <michael.tmv@outlook.com>
2022-01-03 18:40:00 +0200

    notification message

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 var vSlots = Vue.createApp({
     data() {
         return {
-            usernames: ['tvmichael', 'AndriiTorchuk', 'ror6ax']
         }
     }
 })
-.component('github-user-card', {
-    template: '#github-user-card-template',
+.component('notification-message', {
+    template: '#notification-message-template',
     props:{
-        username: {
+        type: {
             type: String,
             required: true,
-            default: ''
+            default: 'info'
+        },
+        header: {
+            type: String
         }
     },
     data() {
         return {
-            user: {},
-            gitApiUrl: 'https://api.github.com/users/',
+            hidden: false
         }
     },
-    async created(){
-        let response = await axios.get(this.gitApiUrl + this.username);
-        this.user = response.data;
+    methods: {
+        hide() {
+            this.hidden = true;
+        }
     }
 })
 .mount('#app');
